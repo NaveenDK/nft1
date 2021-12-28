7c0f115a4455f44355f461c46c1aec5490252c20
NaveenDK <naveenkolambage@gmail.com>
2021-12-28 20:54:44 +1300

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Header.css'
 import punkLogo from '../assets/header/neuralogov2.png';
 import searchIcon from   '../assets/header/search.png';
 import themeSwitchIcon from '../assets/header/theme-switch.png'
 
 const Header = () => {
+
+    const[open,setOpen] = useState(false);
+
+
+
     return (
-        <div className='header'>
-            <div className='logoContainer'>
+        <div className="header" >
+            <nav>
+                <div className="logo">
                 <img src={punkLogo} className='punkLogo' alt="" />
-            </div>
+                </div>
+                <ul className="nav-links" style={{transform: open? "translateX(0px)": ""}}>
+                        <li>
+                                <a>Tutorials</a>
+                        </li>
+                        <li>
+                                <a>Healthcare AI</a>
+                        </li>
+                        <li>
+                                <a>Covid 19</a>
+                        </li>
+                        <li>
+                                <a>Crypto & NFTS</a>
+                        </li>
+                </ul>
+                <i onClick={()=> setOpen(!open)} className="fas fa-bars burger"></i>
 
-        
-        <div className="headerItems">
-     
-            <p>Tutorials</p>
-            <p>Healthcare AI</p>
-            <p>Covid-19</p>
-            <p>Crypto & NFTS</p>
-         
-        </div>
 
-        <div className="headerActions">
+            </nav>
+
+   
+
+        {/* <div className="headerActions">
          <div className="themeSwitchContainer">
              <img src={themeSwitchIcon}/>
          </div>
-        </div>
+        </div> */}
 
-        <div className="loginButton">
+        {/* <div className="loginButton">
             GET IN
-        </div>
+        </div> */}
 
         </div>
          
